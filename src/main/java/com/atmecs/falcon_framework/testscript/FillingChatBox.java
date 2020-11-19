79949ab96796306d45e90eeadd4cedf5b090eb2b
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-11-19 16:03:34 +0530

    Updated

--- src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
+++ src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
@@ -10,29 +10,29 @@ import com.atmecs.falcon.automation.util.reporter.ReportLogService;
 import com.atmecs.falcon.automation.util.reporter.ReportLogServiceImpl;
 import com.atmecs.falcon_framework.testsuite.SampleTestSuiteBase;
 
-public class FillingChatBox extends SampleTestSuiteBase
+public class FillingChatBox extends SampleTestSuiteBase 
 {
-private ReportLogService report = new ReportLogServiceImpl(SampleTestScript.class);
-	
+	private ReportLogService report = new ReportLogServiceImpl(SampleTestScript.class);
+
 	@Test
-	@Parameters({"os", "osVersion", "browser", "browserVersion"})
+	@Parameters({ "os", "osVersion", "browser", "browserVersion" })
 	public void sampleTest(String os, String osVersion, String br, String browserVersion) 
 	{
-		// write your first test script here
-		// sample
-		report.info("Opening browser: "+ br);
+		// Handling Frames
+		report.info("Opening browser: " + br);
 		browser.openURL("https://phptravels.com/demo", os, osVersion, br, browserVersion);
 		report.info("Maximizing browser window");
 		browser.maximizeWindow();
-		/*
-		 * report.info("entering text: "+"Selenium");
-		 * browser.getTextField().enterTextField(LocatorType.NAME, "q", "Selenium");
-		 * report.info("pressing enter key");
-		 * browser.getKey().pressKey(LocatorType.NAME, "q", KeyType.ENTER);
-		 * report.info("waiting for 2 second"); browser.getWait().HardPause(2000);
-		 * String text = browser.getCurrentPageTitle();
-		 * report.info("verifying page title"); Verify.verifyString(text,
-		 * "Selenium - Google Search", "Verifying String Message ");
-		 */
+
+		report.info("entering text: " + "Selenium");
+		browser.getTextField().enterTextField(LocatorType.NAME, "q", "Selenium");
+		report.info("pressing enter key");
+		browser.getKey().pressKey(LocatorType.NAME, "q", KeyType.ENTER);
+		report.info("waiting for 2 second");
+		browser.getWait().HardPause(2000);
+		String text = browser.getCurrentPageTitle();
+		report.info("verifying page title");
+		Verify.verifyString(text, "Selenium - Google Search", "Verifying String Message ");
+
 	}
 }
