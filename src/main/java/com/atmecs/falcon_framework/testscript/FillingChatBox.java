f960648445e17652c6f89606a942e73271104c02
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-11-20 16:04:58 +0530

    Updated

--- src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
+++ src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
@@ -1,8 +1,8 @@
 package com.atmecs.falcon_framework.testscript;
 
+import org.openqa.selenium.By;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
-
 import com.atmecs.falcon.automation.ui.selenium.Verify;
 import com.atmecs.falcon.automation.ui.seleniuminterfaces.IKeys.KeyType;
 import com.atmecs.falcon.automation.util.enums.LocatorType;
@@ -10,10 +10,13 @@ import com.atmecs.falcon.automation.util.reporter.ReportLogService;
 import com.atmecs.falcon.automation.util.reporter.ReportLogServiceImpl;
 import com.atmecs.falcon_framework.testsuite.SampleTestSuiteBase;
 
-public class FillingChatBox extends SampleTestSuiteBase 
+public class FillingChatBox extends SampleTestSuiteBase
 {
 	private ReportLogService report = new ReportLogServiceImpl(SampleTestScript.class);
 
+	String frame = "//iframe[@id='chat-widget']";
+
+	@SuppressWarnings("deprecation")
 	@Test
 	@Parameters({ "os", "osVersion", "browser", "browserVersion" })
 	public void sampleTest(String os, String osVersion, String br, String browserVersion) 
@@ -24,18 +27,34 @@ public class FillingChatBox extends SampleTestSuiteBase
 		report.info("Maximizing browser window");
 		browser.maximizeWindow();
 
-		report.info("Switching to frame: " );
-		browser.getClick().clickButtonByName(LocatorType.TAGNAME, "iframe");
+		report.info("Waiting for sometimes to enable chatbox");
+		browser.getWait().HardPause(10000);
+
+		report.info("Switching to frame");
+		browser.getDriver().switchTo().frame("//iframe[@name='chat-widget']");
 
 		report.info("Clicking chat icon");
-		browser.getClick().doubleClickOnElementByXpath(LocatorType.NAME, "chat-widget");
+		browser.getDriver().findElement(By.xpath("//div[@class='lc-1z06j5z e1dmt1bi1']")).click();
+		
+		report.info("Entering name");
+		browser.getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Abc");
+		
+		report.info("Entering number");
+		browser.getDriver().findElement(By.xpath("(//input[@class='lc-1gz7fd7 e1xplv9i0'])[2]")).sendKeys("9823734854");
+		
+		report.info("Entering mail");
+		browser.getDriver().findElement(By.xpath("//input[@id='email']")).sendKeys("abc@gmail.com");
+		
+		
 		/*
 		 * browser.getKey().pressKey(LocatorType.NAME, "q", KeyType.ENTER);
 		 * report.info("waiting for 2 second"); browser.getWait().HardPause(2000);
+		 * 
+		 * 
+		 * String text = browser.getCurrentPageTitle();
+		 * report.info("verifying page title"); Verify.verifyString(text, "phptravels",
+		 * "Verifying String Message ");
 		 */
-		String text = browser.getCurrentPageTitle();
-		report.info("verifying page title");
-		Verify.verifyString(text, "phptravels", "Verifying String Message ");
 
 	}
 }
