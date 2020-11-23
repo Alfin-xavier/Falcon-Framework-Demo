ab8400e3aa4c31004cac1d02f8394f56f3cd2ddc
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-11-23 16:25:02 +0530

    Updated

--- src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
+++ src/main/java/com/atmecs/falcon_framework/testscript/FillingChatBox.java
@@ -3,24 +3,23 @@ package com.atmecs.falcon_framework.testscript;
 import org.openqa.selenium.By;
 import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
+
+import com.atmecs.falcon.automation.ui.selenium.Find;
+import com.atmecs.falcon.automation.ui.selenium.PageHelper;
 import com.atmecs.falcon.automation.ui.selenium.Verify;
 import com.atmecs.falcon.automation.ui.seleniuminterfaces.IKeys.KeyType;
+import com.atmecs.falcon.automation.ui.seleniuminterfaces.WindowHandler.FrameHandler;
 import com.atmecs.falcon.automation.util.enums.LocatorType;
 import com.atmecs.falcon.automation.util.reporter.ReportLogService;
 import com.atmecs.falcon.automation.util.reporter.ReportLogServiceImpl;
 import com.atmecs.falcon_framework.testsuite.SampleTestSuiteBase;
 
-public class FillingChatBox extends SampleTestSuiteBase
-{
+public class FillingChatBox extends SampleTestSuiteBase {
 	private ReportLogService report = new ReportLogServiceImpl(SampleTestScript.class);
 
-	String frame = "//iframe[@id='chat-widget']";
-
-	@SuppressWarnings("deprecation")
 	@Test
 	@Parameters({ "os", "osVersion", "browser", "browserVersion" })
-	public void sampleTest(String os, String osVersion, String br, String browserVersion) 
-	{
+	public void sampleTest(String os, String osVersion, String br, String browserVersion) throws InterruptedException {
 		// Handling Frames
 		report.info("Opening browser: " + br);
 		browser.openURL("https://phptravels.com/demo", os, osVersion, br, browserVersion);
@@ -28,24 +27,35 @@ public class FillingChatBox extends SampleTestSuiteBase
 		browser.maximizeWindow();
 
 		report.info("Waiting for sometimes to enable chatbox");
-		browser.getWait().HardPause(10000);
+		browser.getWait().safeWait(10000);
 
 		report.info("Switching to frame");
-		browser.getDriver().switchTo().frame("//iframe[@name='chat-widget']");
+		String frame = "//iframe[@id='chat-widget']";
+		browser.getWindowHandler().handleFrame().switchToFrame(frame );
+		//browser.getDriver().switchTo().frame("//iframe[@id='chat-widget']");
+		//browser.getWindowHandler().handleFrame().switchToFrame("//iframe[@id='chat-widget']");
+		report.info("Switched successfully");
 
 		report.info("Clicking chat icon");
-		browser.getDriver().findElement(By.xpath("//div[@class='lc-1z06j5z e1dmt1bi1']")).click();
-		
+		browser.getClick().clickButtonByName(LocatorType.XPATH, "//div[@class='lc-xqoq0v e16i86ec0']");
+		//browser.getFindFromBrowser().findElementByClassName("lc-1z06j5z e1dmt1bi1").click();
+		//browser.getDriver().findElement(By.xpath("//div[@class='lc-1z06j5z e1dmt1bi1']")).click();
+		  
 		report.info("Entering name");
-		browser.getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Abc");
-		
+		browser.getTextField().enterTextField(LocatorType.XPATH, "//input[@id='name']", "Abc");
+		// browser.getDriver().findElement(By.xpath("//input[@id='name']")).sendKeys("Abc");
+
+		String number = "//input[@id='8jc8ik9hqge_157564443267307015']";
 		report.info("Entering number");
-		browser.getDriver().findElement(By.xpath("(//input[@class='lc-1gz7fd7 e1xplv9i0'])[2]")).sendKeys("9823734854");
-		
+		browser.getTextField().enterTextField(LocatorType.XPATH, number, "9823734854");
+		// browser.getDriver().findElement(By.xpath("(//input[@class='lc-1gz7fd7e1xplv9i0'])[2]")).sendKeys("9823734854");
+
 		report.info("Entering mail");
 		browser.getDriver().findElement(By.xpath("//input[@id='email']")).sendKeys("abc@gmail.com");
 		
-		
+		report.info("Click dropdown");
+		browser.getClick().performClick(LocatorType.XPATH, "//div[@class='lc-sgcxhz egtcv0s0']");
+			  
 		/*
 		 * browser.getKey().pressKey(LocatorType.NAME, "q", KeyType.ENTER);
 		 * report.info("waiting for 2 second"); browser.getWait().HardPause(2000);
@@ -54,7 +64,8 @@ public class FillingChatBox extends SampleTestSuiteBase
 		 * String text = browser.getCurrentPageTitle();
 		 * report.info("verifying page title"); Verify.verifyString(text, "phptravels",
 		 * "Verifying String Message ");
+		 * 
+		 * 
 		 */
-
 	}
 }
