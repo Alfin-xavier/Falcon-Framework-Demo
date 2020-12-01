11ab39c802fbe9eaa576dea49c82cde30d4be109
ATMECS\Alfin.Anthonyraj <alfin.anthonyraj@atmecs.com>
2020-12-01 20:44:27 +0530

    Updated

---
+++ src/main/java/com/atmecs/falcon_framework/pages/TestDataObject.java
@@ -0,0 +1,36 @@
+package com.atmecs.falcon_framework.pages;
+
+public class TestDataObject 
+{
+	String userName, mobileNumber, userMail;
+
+	public String getUserName() 
+	{
+		return userName;
+	}
+
+	public void setUserName(String userName) 
+	{
+		this.userName = userName;
+	}
+
+	public String getMobileNumber() 
+	{
+		return mobileNumber;
+	}
+
+	public void setMobileNumber(String mobileNumber)
+	{
+		this.mobileNumber = mobileNumber;
+	}
+
+	public String getUserMail() 
+	{
+		return userMail;
+	}
+
+	public void setUserMail(String userMail) 
+	{
+		this.userMail = userMail;
+	}
+}
